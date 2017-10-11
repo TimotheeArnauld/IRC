39c4b0d586ff43428209fe0de2a7a8526d657463
Yukenigon <timothee.arnauld1@gmail.com>
2017-10-11 11:02:52 +0200

    Client stylesheet

--- src/client/main.c
+++ src/client/main.c
@@ -11,9 +11,27 @@ int main(int argc, char** argv){
     if(connect(socket_desc, (struct sockaddr*)&server, sizeof(server)) < 0)
         return -1;
 
+    menu();
     chat();
-
     return 0;
+}
+
+void menu(){
+    system("clear");
+    printf("%s\
+            %s\n", format_string(SOFTWARE_NAME, 2, BOLD, BLUE), format_string(VERSION, 2, BOLD, GREEN));
+
+    printf("%s\n\
+            %s\n", format_string(COPYRIGHT_MESS,2, BOLD, YELLOW ), format_string(WELCOME_MESS, 2, UNDERLINED, RED));
+
+    available_commands();
+}
+
+void available_commands(){
+    __return__(3);
+    printf("%s\n", format_string(AVAILABLE_DISPLAY, 2, WHITE, NORMAL));
+    __return__(1);
+    printf("%s\n", format_string(AVAILABLE_COMMANDS, 2, WHITE, NORMAL));
 }
 
 void chat(){
@@ -21,8 +39,12 @@ void chat(){
     
     while(1){
        char s[2000];
-       printf("Nickname: ");
+       printf("%s\n", format_string("Nickname: ", 1, CYAN));
+       printf("%s", format_string("", 2, WHITE, NORMAL));
        scanf("%s", s);
+       if(strcmp(s, "quit") == 0){
+        exit(0);
+       }
        send(socket_desc, s, strlen(s), 0);
     }
 
--- src/client/main.h
+++ src/client/main.h
@@ -6,6 +6,18 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#include "../lib/fonts/font.h"
+
+#define SOFTWARE_NAME "IRC free client"
+#define VERSION "V.0.0.1"
+#define WELCOME_MESS "Welcome in IRC - Home"
+#define COPYRIGHT_MESS "Copyright 2017-2018 © All rights reserved - ESGI"
+
+#define AVAILABLE_DISPLAY "Available Commands:"
+#define AVAILABLE_COMMANDS "quit: Exit the program\n"\
+    "connect: Start the server\n"\
+    "help: Show available commands.\n"
+
 #define SOCKET_IP_ADDRESS "127.0.0.1"
 #define SOCKET_PORT 1330
 
@@ -14,6 +26,7 @@
 int socket_desc;
 struct sockaddr_in server;
 char server_reply[10000];
-
+void menu();
+void available_commands();
 void chat();
 
