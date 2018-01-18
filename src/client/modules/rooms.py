15129d1df9f8a2373cd975745a4a474436c238a9
Yousria <yousria.benchadi@gmail.com>
2018-01-18 21:37:03 +0100

    allow user to create a room if it doesn't exit in the DB

--- src/client/modules/rooms.py
+++ src/client/modules/rooms.py
@@ -2,6 +2,7 @@
 import psycopg2
 import sys
 
+#return a list of the rooms space separated 
 def see_available_rooms():
 	conn = psycopg2.connect("dbname=docker user=docker host=localhost password=docker")
 	cur = conn.cursor()
@@ -18,6 +19,28 @@ def see_available_rooms():
 			return rooms
 	except (Exception, psycopg2.DatabaseError) as error:
 		return "nothing"
+	finally:
+		if conn is not None:
+			cur.close()
+			conn.close()
+
+#return true if it's created and false if it exists in the database
+def create_room(name):
+	conn = psycopg2.connect("dbname=docker user=docker host=localhost password=docker")
+	cur = conn.cursor()
+	try:
+		sql = "SELECT * FROM rooms WHERE room_name='"+name+"';"
+		cur.execute(sql)
+		if cur.rowcount == 0:
+			sql2 = "INSERT INTO rooms(room_name) VALUES(%s);"
+			cur.execute(sql2, (name,))
+			conn.commit()
+			return "true"
+		else:
+			return "false"
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+		return "false"
 	finally:
 		if conn is not None:
 			cur.close()
