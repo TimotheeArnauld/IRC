4c59b6fdf9c4a6a1cfe6ed8170a2e57b7f4ef04c
uk4 <timothee.arnauld1@gmail.com>
2018-01-20 14:15:45 +0100

    update

--- src/client/command.c
+++ src/client/command.c
@@ -1,14 +1,14 @@
 #include "command.h"
 
 char* command_analyse(char *s){
-	if(strcmp(command, "-1") == 0){
+	if(strcmp(s, "-1") == 0){
 		return "-1";
 	}else{
-		if(strcmp(command, "0") == 0){
+		if(strcmp(s, "0") == 0){
 			exit(0);
 		}
 		else{
-			return command;
+			return s;
 		}
 	}
 
